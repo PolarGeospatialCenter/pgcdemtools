3a913d759bcf0924710d4341cf057523b2726ae1
porte254 <porte254@umn.edu>
2021-04-15 09:54:13 -0500

    Repair reverted changes in utils. Repair CRLF.

--- lib/utils.py
+++ lib/utils.py
@@ -7,9 +7,10 @@ demtools utilties and constants
 import logging
 import os
 import re
+import numpy as np
 from collections import namedtuple
 
-from osgeo import osr, ogr
+from osgeo import osr, ogr, gdalconst, gdal
 
 #### Create Logger
 logger = logging.getLogger("logger")
@@ -17,51 +18,50 @@ logger.setLevel(logging.DEBUG)
 
 # Copy DEM global vars
 deliv_suffixes = (
-### ASP
-'-DEM.prj',
-'-DEM.tif',
-'-DRG.tif',
-'-IntersectionErr.tif',
-'-GoodPixelMap.tif',
-'-stereo.default',
-'-PC.laz',
-'-PC.las',
-'.geojson',
+    ### ASP
+    '-DEM.prj',
+    '-DEM.tif',
+    '-DRG.tif',
+    '-IntersectionErr.tif',
+    '-GoodPixelMap.tif',
+    '-stereo.default',
+    '-PC.laz',
+    '-PC.las',
+    '.geojson',
 
-### SETSM
-'_dem.tif',
-'_ortho.tif',
-'_matchtag.tif',
-'_meta.txt'
+    ### SETSM
+    '_dem.tif',
+    '_ortho.tif',
+    '_matchtag.tif',
+    '_meta.txt'
 )
 
 archive_suffix = ".tar"
 
 shp_suffixes = (
-'.shp',
-'.shx',
-'.prj',
-'.dbf'
+    '.shp',
+    '.shx',
+    '.prj',
+    '.dbf'
 )
 
 pc_suffixes = (
-'-PC.tif',
-'-PC-center.txt'
+    '-PC.tif',
+    '-PC-center.txt'
 )
 
 fltr_suffixes = (
-'_fltr-DEM.tif',
-'_fltr-DEM.prj'
+    '_fltr-DEM.tif',
+    '_fltr-DEM.prj'
 )
 
-
 log_suffixes = (
-'-log-point2dem',
-'-log-stereo_corr',
- '-log-stereo_pprc',
-'-log-stereo_fltr',
-'-log-stereo_rfne',
-'-log-stereo_tri',
+    '-log-point2dem',
+    '-log-stereo_corr',
+    '-log-stereo_pprc',
+    '-log-stereo_fltr',
+    '-log-stereo_rfne',
+    '-log-stereo_tri',
 )
 
 # common name id, attribute field name, storage type, field width, field precision
@@ -200,7 +200,7 @@ OVERLAP_FILE_BASIC_ATTRIBUTE_DEFINITIONS = [
     StandardAttribute("OVERLAP", ogr.OFTString, 254, 0),
     StandardAttribute("PAIRNAME", ogr.OFTString, 64, 0),
     StandardAttribute("STATUS", ogr.OFTInteger, 2, 0)
-    ]
+]
 
 OVERLAP_FILE_ADDITIONAL_ATTRIBUTE_DEFINITIONS = [
 
@@ -231,9 +231,45 @@ OVERLAP_FILE_ADDITIONAL_ATTRIBUTE_DEFINITIONS = [
     StandardAttribute("CR_DATE", ogr.OFTString, 32, 0),
     StandardAttribute("RUNTIME", ogr.OFTReal, 0, 0),
     StandardAttribute("DEM_NAME", ogr.OFTString, 254, 0)
-    ]
+]
 
 OVERLAP_FILE_ATTRIBUTE_DEFINITIONS = OVERLAP_FILE_BASIC_ATTRIBUTE_DEFINITIONS + OVERLAP_FILE_ADDITIONAL_ATTRIBUTE_DEFINITIONS
+
+
+class RasterIOError(Exception):
+    def __init__(self, msg=""):
+        super(Exception, self).__init__(msg)
+
+
+class SpatialRef(object):
+
+    def __init__(self, epsg):
+        srs = osr_srs_preserve_axis_order(osr.SpatialReference())
+        try:
+            epsgcode = int(epsg)
+
+        except ValueError:
+            raise RuntimeError("EPSG value must be an integer: %s" % epsg)
+        else:
+
+            err = srs.ImportFromEPSG(epsgcode)
+            if err == 7:
+                raise RuntimeError("Invalid EPSG code: %d" % epsgcode)
+            else:
+                proj4_string = srs.ExportToProj4()
+
+                proj4_patterns = {
+                    "+ellps=GRS80 +towgs84=0,0,0,0,0,0,0": "+datum=NAD83",
+                    "+ellps=WGS84 +towgs84=0,0,0,0,0,0,0": "+datum=WGS84",
+                }
+
+                for pattern, replacement in proj4_patterns.items():
+                    if proj4_string.find(pattern) != -1:
+                        proj4_string = proj4_string.replace(pattern, replacement)
+
+                self.srs = srs
+                self.proj4 = proj4_string
+                self.epsg = epsgcode
 
 
 def osr_srs_preserve_axis_order(osr_srs):
@@ -242,37 +278,6 @@ def osr_srs_preserve_axis_order(osr_srs):
     except AttributeError:
         pass
     return osr_srs
-
-
-class SpatialRef(object):
-
-    def __init__(self,epsg):
-        srs = osr_srs_preserve_axis_order(osr.SpatialReference())
-        try:
-            epsgcode = int(epsg)
-
-        except ValueError as e:
-            raise RuntimeError("EPSG value must be an integer: %s" %epsg)
-        else:
-
-            err = srs.ImportFromEPSG(epsgcode)
-            if err == 7:
-                raise RuntimeError("Invalid EPSG code: %d" %epsgcode)
-            else:
-                proj4_string = srs.ExportToProj4()
-
-                proj4_patterns = {
-                    "+ellps=GRS80 +towgs84=0,0,0,0,0,0,0":"+datum=NAD83",
-                    "+ellps=WGS84 +towgs84=0,0,0,0,0,0,0":"+datum=WGS84",
-                }
-
-                for pattern, replacement in proj4_patterns.items():
-                    if proj4_string.find(pattern) != -1:
-                        proj4_string = proj4_string.replace(pattern,replacement)
-
-                self.srs = srs
-                self.proj4 = proj4_string
-                self.epsg = epsgcode
 
 
 def check_file_inclusion(f, pairname, overlap_prefix, args):
@@ -299,7 +304,7 @@ def check_file_inclusion(f, pairname, overlap_prefix, args):
                 move_file = True
 
         if args.exclude_drg is True:
-            if f.endswith(('-DRG.tif','_ortho.tif')):
+            if f.endswith(('-DRG.tif', '_ortho.tif')):
                 move_file = False
 
         if args.exclude_err is True:
@@ -308,9 +313,9 @@ def check_file_inclusion(f, pairname, overlap_prefix, args):
 
         if args.dems_only is True:
             move_file = False
-            if f.endswith(("-DEM.tif",'-DEM.prj','.geojson','_dem.tif','_meta.txt')):
+            if f.endswith(("-DEM.tif", '-DEM.prj', '.geojson', '_dem.tif', '_meta.txt')):
                 move_file = True
-            if f.endswith(("_fltr-DEM.tif",'_fltr-DEM.prj')):
+            if f.endswith(("_fltr-DEM.tif", '_fltr-DEM.prj')):
                 if args.include_fltr:
                     move_file = True
                 else:
@@ -322,7 +327,7 @@ def check_file_inclusion(f, pairname, overlap_prefix, args):
                 move_file = True
 
     #### determine if file is in pair shp
-    if (f.endswith(shp_suffixes) and pairname in f and not '-DEM' in f):
+    if f.endswith(shp_suffixes) and pairname in f and '-DEM' not in f:
         if not args.dems_only:
             move_file = True
 
@@ -341,7 +346,7 @@ def get_source_names(src_fp):
         msg = "The source {} does not appear to be a shapefile or File GDB -- quitting".format(src_fp)
         raise RuntimeError(msg)
 
-    return (_src_fp, src_lyr)
+    return _src_fp, src_lyr
 
 
 def get_source_names2(src_str):
@@ -367,10 +372,11 @@ def get_source_names2(src_str):
         pfx, src_ds, src_lyr = src_str.split(":")
 
     else:
-        msg = "The source {} does not appear to be a Shapefile, File GDB, or PostgreSQL connection -- quitting".format(src_str)
+        msg = "The source {} does not appear to be a Shapefile, File GDB, or PostgreSQL connection -- quitting".format(
+            src_str)
         raise RuntimeError(msg)
 
-    return (driver, src_ds, src_lyr)
+    return driver, src_ds, src_lyr
 
 
 def drange(start, stop, step):
@@ -378,3 +384,46 @@ def drange(start, stop, step):
     while r < stop:
         yield r
         r += step
+
+
+def gdalReadAsArraySetsmSceneBand(raster_band, make_nodata_nan=False):
+    scale = raster_band.GetScale()
+    offset = raster_band.GetOffset()
+    if scale is None:
+        scale = 1.0
+    if offset is None:
+        offset = 0.0
+    if scale == 1.0 and offset == 0.0:
+        array_data = raster_band.ReadAsArray()
+        if make_nodata_nan:
+            nodata_val = raster_band.GetNoDataValue()
+            if nodata_val is not None:
+                array_data[array_data == nodata_val] = np.nan
+    else:
+        if raster_band.DataType != gdalconst.GDT_Int32:
+            raise RasterIOError(
+                "Expected GDAL raster band with scale!=1.0 or offset!=0.0 to be of Int32 data type"
+                " (scaled int LERC_ZSTD-compressed 50cm DEM), but data type is {}".format(
+                    gdal.GetDataTypeName(raster_band.DataType)
+                )
+            )
+        if scale == 0.0:
+            raise RasterIOError(
+                "GDAL raster band has invalid parameters: scale={}, offset={}".format(scale, offset)
+            )
+        nodata_val = raster_band.GetNoDataValue()
+        array_data = raster_band.ReadAsArray(buf_type=gdalconst.GDT_Float32)
+        adjust_where = (array_data != nodata_val) if nodata_val is not None else True
+        if scale != 1.0:
+            np.multiply(array_data, scale, out=array_data, where=adjust_where)
+        if offset != 0.0:
+            np.add(array_data, offset, out=array_data, where=adjust_where)
+        if make_nodata_nan:
+            array_nodata = np.logical_not(adjust_where, out=adjust_where)
+            array_data[array_nodata] = np.nan
+        del adjust_where
+
+    if array_data is None:
+        raise RasterIOError("`raster_band.ReadAsArray()` returned None")
+
+    return array_data
