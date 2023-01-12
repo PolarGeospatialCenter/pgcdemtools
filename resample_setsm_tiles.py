22a9007d483993ea5b8eb59f17ea7c89455e2666
Erik Husby <husby036@umn.edu>
2023-01-12 14:16:11 +1300

    Change gdal_translate calls to gdalwarp because `-ovr NONE` option for gdal_translate is only available in GDAL version 3.6+

--- resample_setsm_tiles.py
+++ resample_setsm_tiles.py
@@ -369,7 +369,7 @@ def merge_rasters(inputps, output, component, args):
                 '-co compress=lzw -co predictor={}'.format(predictor)
 
     cos = cos_cog if args.output_cogs else cos_gtiff
-    cmd = 'gdal_translate -q -ovr NONE {} "{}" "{}"'.format(
+    cmd = 'gdalwarp -q -ovr NONE {} "{}" "{}"'.format(
         cos, vrt, output
     )
 
@@ -429,7 +429,7 @@ def process_raster(inputp, output, component, args):
 
                 if args.output_cogs:
                     # Convert gdal_calc.py GTiff output to COG format
-                    cmd = 'gdal_translate -q -ovr NONE {2} "{0}" "{1}"'.format(
+                    cmd = 'gdalwarp -q -ovr NONE {2} "{0}" "{1}"'.format(
                         output_tmp, output, cos_cog
                     )
                     logger.debug(cmd)
