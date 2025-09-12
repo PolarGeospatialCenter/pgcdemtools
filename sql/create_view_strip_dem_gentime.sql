f4ad92673162dad9122cd900be9cae135974b98d
Claire Porter <porte254@umn.edu>
2025-09-12 13:51:45 -0500

    Update DB view definitions

--- sql/create_view_strip_dem_gentime.sql
+++ sql/create_view_strip_dem_gentime.sql
@@ -1,4 +1,4 @@
-create view strip_dem_gentime(stripdemid, is_dsp, min_gen_time, max_gen_time) as
+create view dem.strip_dem_gentime(stripdemid, is_dsp, min_gen_time, max_gen_time) as
 SELECT scene_dem_all.stripdemid,
        scene_dem_all.is_dsp,
        min(LEAST(scene_dem_all.gen_time1, scene_dem_all.gen_time2))    AS min_gen_time,
@@ -6,7 +6,7 @@ SELECT scene_dem_all.stripdemid,
 FROM dem.scene_dem_all
 GROUP BY scene_dem_all.stripdemid, scene_dem_all.is_dsp;
 
-alter table strip_dem_gentime
+alter table dem.strip_dem_gentime
     owner to pgc_gis_admin;
 
-grant select on strip_dem_gentime to pgc_users;
+grant select on dem.strip_dem_gentime to pgc_users;
